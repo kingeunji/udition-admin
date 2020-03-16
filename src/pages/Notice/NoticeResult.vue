e13232bc899628bd482b6a47835d8121dbd895fd
Linda324 <blackholic324@gmail.com>
2020-03-16 18:20:01 +0900

    #2 공지사항 리스트 불러오기

---
+++ src/pages/Notice/NoticeResult.vue
@@ -0,0 +1,113 @@
+<template>
+    <div>
+        <el-table ref="multiTable" :data="noticeList" v-loading="loading" style="width:100%">
+
+            <el-table-column label="#" width="80" align="center">
+                <template slot-scope="scope">
+                    {{ scope.row.noticeNo }}
+                </template>
+            </el-table-column>
+
+            <el-table-column label="제목" width="400" align="center">
+                <template slot-scope="scope">
+                <div>
+                    <b>{{ scope.row.title }}</b>
+                </div>
+                </template>
+            </el-table-column>
+
+            <el-table-column align="center">
+                <template slot-scope="scope">
+                <div style="padding: 30px;">
+                    <el-button size="mini" @click="deleteDialog(scope.row.noticeNo)">삭제</el-button>
+                </div>
+                </template>
+            </el-table-column>
+        </el-table>
+
+        <div style="text-align: center; margin-top: 15px;">
+            <el-pagination
+                background
+                layout="prev, pager, next"
+                :total="this.pagination.dbCount"
+                @current-change="pageChange"
+                @next-click="pageChange"
+                @prev-click="pageChange">
+            </el-pagination>
+        </div>
+
+
+
+
+        <el-dialog
+            title="알림"
+            :visible.sync="dialogVisible"
+            width="30%">
+            <div class="detail_pannel">
+                <span>
+                    해당 공지사항을 삭제하겠습니까? 
+                </span>
+                
+            </div>
+            
+            <span slot="footer" class="dialog-footer">
+                <el-button style="width: auto" @click="dialogVisible = false">취소</el-button>
+                <el-button style="width: auto" type="primary" @click="deleteNotice">삭제</el-button>
+            </span>
+        </el-dialog>
+
+    </div>
+</template>
+
+<script>
+import {notice} from '../../api/notice'
+
+export default {
+    data() {
+        return {
+            noticeList : {},
+            loading : true,
+            pagination : '',
+            formData : {
+                requestPage : 0
+            },
+            dialogVisible : false,
+            noticeNo : ''
+        }
+    },
+    created() {
+        this.fetchData()
+    },
+    methods : {
+        fetchData() {
+            notice.search(this.formData)
+                  .then(data => {
+                      this.noticeList = data.results
+                      this.pagination = data.page
+                      this.loading = false
+                  })
+        },
+        deleteDialog(noticeNo) {
+            this.dialogVisible = true
+            this.noticeNo = noticeNo
+        },
+        deleteNotice() {
+            notice.delete(this.noticeNo) 
+                  .then(data => {
+                      if(data.status.code == 0) {
+                          this.$message("공지사항이 삭제되었습니다")
+                          this.dialogVisible = false
+                          this.fetchData()
+                      }
+                  })
+        },
+        pageChange(val) {
+            this.formData.requestPage = (val-1)
+            this.fetchData()
+        },
+    }
+};
+</script>
+
+<style>
+</style>
