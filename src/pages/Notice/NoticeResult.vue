9df9ec47815d0fc88209858df592d4ef33ead11d
Linda324 <blackholic324@gmail.com>
2020-03-17 12:40:42 +0900

    공지사항 상세 내용 보기

--- src/pages/Notice/NoticeResult.vue
+++ src/pages/Notice/NoticeResult.vue
@@ -11,17 +11,18 @@
             <el-table-column label="제목" width="400" align="center">
                 <template slot-scope="scope">
                 <div>
-                    <b>{{ scope.row.title }}</b>
+                    <b v-on:click="noticeDetail(scope.row)">{{ scope.row.title }}</b>
                 </div>
                 </template>
             </el-table-column>
 
             <el-table-column align="center">
-                <template slot-scope="scope">
+                <span slot-scope="scope">
                 <div style="padding: 30px;">
+                    <el-button size="mini">수정</el-button>
                     <el-button size="mini" @click="deleteDialog(scope.row.noticeNo)">삭제</el-button>
                 </div>
-                </template>
+                </span>
             </el-table-column>
         </el-table>
 
@@ -36,12 +37,39 @@
             </el-pagination>
         </div>
 
+        <el-dialog width="65%" :visible.sync="dialogVisible" title="공지사항" >
 
+            <div class="detail_pannel">
+                <div class="row loop-row">
+                    <div class="col-9 col-lg-2">
+                       <b> 번호 </b>
+                    </div>
+                    <div class="col-md-11 col-lg-9">
+                        {{ noticeInfo.noticeNo }}
+                    </div>
+                </div>
+                <div class="row loop-row">
+                    <div class="col-9 col-lg-2">
+                       <b> 제목 </b>
+                    </div>
+                    <div class="col-md-11 col-lg-9">
+                        {{ noticeInfo.title }}
+                    </div>
+                </div>
+                <div class="row loop-row">
+                    <div class="col-9 col-lg-2">
+                       <b> 내용 </b>
+                    </div>
+                    <div class="col-md-11 col-lg-9">
+                        {{ noticeInfo.tts }}
+                    </div>
+                </div>
+            </div>
 
-
+        </el-dialog>
         <el-dialog
             title="알림"
-            :visible.sync="dialogVisible"
+            :visible.sync="dialogVisible2"
             width="30%">
             <div class="detail_pannel">
                 <span>
@@ -51,10 +79,12 @@
             </div>
             
             <span slot="footer" class="dialog-footer">
-                <el-button style="width: auto" @click="dialogVisible = false">취소</el-button>
+                <el-button style="width: auto" @click="dialogVisible2 = false">취소</el-button>
                 <el-button style="width: auto" type="primary" @click="deleteNotice">삭제</el-button>
             </span>
         </el-dialog>
+
+
 
     </div>
 </template>
@@ -65,14 +95,21 @@ import {notice} from '../../api/notice'
 export default {
     data() {
         return {
-            noticeList : {},
+            noticeList : [],
             loading : true,
             pagination : '',
             formData : {
                 requestPage : 0
             },
             dialogVisible : false,
-            noticeNo : ''
+            dialogVisible2 : false,
+            innerVisivle : false,
+            noticeInfo : {
+                noticeNo : '',
+                title : '',
+                tts : ''
+            }
+            
         }
     },
     created() {
@@ -88,7 +125,7 @@ export default {
                   })
         },
         deleteDialog(noticeNo) {
-            this.dialogVisible = true
+            this.dialogVisible2 = true
             this.noticeNo = noticeNo
         },
         deleteNotice() {
@@ -105,6 +142,12 @@ export default {
             this.formData.requestPage = (val-1)
             this.fetchData()
         },
+        noticeDetail(detail) {
+            this.dialogVisible = true,
+            this.noticeInfo.noticeNo = detail.noticeNo,
+            this.noticeInfo.title = detail.title,
+            this.noticeInfo.tts = detail.tts
+        }
     }
 };
 </script>
