5a25fa1b4abad49aa711573fcf02eaf8f7b7f05b
Linda324 <blackholic324@gmail.com>
2020-04-16 12:20:06 +0900

    #33 배너 파일

---
+++ src/api/banner.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const instance = axios.create({
+    baseURL: 'https://webapi.udition.co/api/banner',
+});
+
+export const banner = {
+    list(bannerType, form) {
+        return instance.post(`/${bannerType}/search`, form);
+    },
+    auditionList(bannerType) {
+        return instance.post(`/${bannerType}/auditionList`, {});
+    },
+    insert(bannerType, form) {
+        return instance.post(`/${bannerType}/insert`, form);
+    }
+}
