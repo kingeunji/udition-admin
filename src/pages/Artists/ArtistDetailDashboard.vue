bd6932f8baa0b33695f11456f2be0f3ab6bc0958
Linda324 <blackholic324@gmail.com>
2020-06-02 18:07:34 +0900

    #66 아티스트 프로필 오류 수정

--- src/pages/Artists/ArtistDetailDashboard.vue
+++ src/pages/Artists/ArtistDetailDashboard.vue
@@ -1,46 +1,52 @@
 <template>
-  <div class="row">
+    <div class="row">
         <div class="col-xl-4 col-lg-5 col-md-6">
-          <card class="card-user">
-            <div slot="image">
-                <el-image :src="'https://storage.googleapis.com/udition-web/fileFolder/' + detailInfo.image" alt="..."> </el-image>
-            </div>
-            <div>
-                <div class="author">
-                    <div class="avatar" alt="..."> </div>
-                    <h4 class="title">{{ detailInfo.fullName }}
-                    <br>
-                    <small>@{{ detailInfo.stageName }}</small>
-                    </h4>
+            <card class="card-user">
+                <div slot="image">
+                    <img id="myImage" :src=" 'https://storage.googleapis.com/udition-web/fileFolder/' + detailInfo.image" alt="..." @error="imageLoadOnError"/>
+                    <!-- <img id="myImage" :src=" 'https://storage.googleapis.com/udition-web/fileFolder/' + detailInfo.image" alt="..." /> -->
+                    <!-- <el-image v-bind:src=" 'data:jpg;base64,'.base64_encode(file_get_contents('https://storage.googleapis.com/udition-web/fileFolder/' + detailInfo.image))" alt="..."> </el-image> -->
+                    <!-- <canvas id="sketch"></canvas> -->
                 </div>
-                <p class="description text-center">
-                    {{ detailInfo.intro }}
-                </p>
-            </div>
-            <hr>
-            <div class="text-center">
-                <div class="row">
-                    <div class="col-lg-3 offset-lg-1">
-                        <h5> {{ detailInfo.viewCnt}}  <br>
-                        <small>조회수</small>
-                        </h5>
+                <div>
+                    <div class="author">
+                        <div class="avatar" alt="..."> </div>
+                        <h4 class="title">{{ detailInfo.fullName }}
+                        <br>
+                        <small>@{{ detailInfo.stageName }}</small>
+                        </h4>
                     </div>
-                    <div class="col-lg-4">
-                        <h5>{{ detailInfo.shareCnt }} <br>
-                        <small> 공유수 </small>
-                        </h5>
-                    </div>
-                    <div class="col-lg-3">
-                        <h5>{{ detailInfo.pokeCnt }}  <br>
-                        <small> 북마크수 </small>
-                        </h5>
+                    <p class="description text-center">
+                        {{ detailInfo.intro }}
+                    </p>
+                </div>
+                <hr>
+                <div class="text-center">
+                    <div class="row">
+                        <div class="col-lg-3 offset-lg-1">
+                            <h5> {{ detailInfo.viewCnt}}  <br>
+                            <small>조회수</small>
+                            </h5>
+                        </div>
+                        <div class="col-lg-4">
+                            <h5>{{ detailInfo.shareCnt }} <br>
+                            <small> 공유수 </small>
+                            </h5>
+                        </div>
+                        <div class="col-lg-3">
+                            <h5>{{ detailInfo.pokeCnt }}  <br>
+                            <small> 북마크수 </small>
+                            </h5>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </card>
-      </div>
+            </card>
 
-      <div class="col-xl-8 col-lg-7 col-md-6">
+            <!-- <el-button style="width:360px; height: 50px; font-size: large;" type="primary" @click="makePDF">프로필 PDF 다운로드</el-button> -->
+
+        </div>
+
+        <div class="col-xl-8 col-lg-7 col-md-6">
             <card class="card">
                 <el-tabs type="card">
                     <el-tab-pane label="Profile">
@@ -56,16 +62,25 @@
 
                 
             </card>
-      </div>
+        </div>
 
-  </div>
+    </div>
 </template>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bluebird/3.7.2/bluebird.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
+<script src="https://unpkg.com/html2canvas@1.0.0-rc.5/dist/html2canvas.js"></script>
 
 <script>
 import {artist} from '../../api/artist.js'
 import Profile from './Profile.vue'
 import Career from './Career.vue'
 import Collection from './Collection.vue'
+import image from '../../assets/img/img_default_user@3x.png'
+
+import html2canvas from 'html2canvas'
+import jsPDF from 'jspdf'
+import CardVue from '../../components/Cards/Card.vue'
 
 
 export default {
@@ -82,7 +97,8 @@ export default {
             collectionForm : {
                 requestType : 'collection',
                 webUrl : ''
-            }
+            },
+            error : '',
         }
     },
     created() {
@@ -102,6 +118,56 @@ export default {
                       this.error = err.data
                   })
         },
+        imageLoadOnError(e) {
+            e.target.src = image
+        },
+        // makePDF(){
+        //   html2canvas(document.querySelector(".content"), {
+        //         allowTaint : true,
+        //         useCORS : true,
+        //   }).then(function(canvas) {
+        //         var originImage = document.getElementById("myImage").getAttribute('src');
+        //         console.log("originImage= " + originImage);
+        //         var encode = window.btoa(originImage);
+        //         console.log("encode= " + encode);
+        //         document.getElementById("myImage").setAttribute('src', 'data:image/png;base64,' + encode);
+        //         var imgData = canvas.toDataURL('image/png');
+        //         var imgWidth = 210;
+        //         var pageHeight = imgWidth * 1.414;
+        //         var imgHeight = canvas.height * imgWidth / canvas.width;
+        //         var doc = new jsPDF('p', 'mm', 'a4');
+
+        //         doc.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+        //         doc.save("artist_profile.pdf");
+        //      document.getElementById("myImage").setAttribute('src', originImage);
+        //     });
+        // },
+        
+        
+        // makePDF() {
+        //     function downloadURI(uri, name){
+        //         var link = document.createElement("a");
+        //         link.download = name;
+        //         link.href = uri;
+        //         document.body.appendChild(link);
+        //         link.click();
+        //     }
+        //     html2canvas(document.querySelector(".content"), {
+        //         useCORS : true,
+        //     }).then(function(canvas) {
+                
+        //         var originImage = document.getElementById("myImage").getAttribute('src');
+        //         console.log("originImage= " + originImage);
+        //         var encode = window.btoa(originImage);
+        //         console.log("encode= " + encode);
+        //         document.getElementById("myImage").setAttribute('src', 'data:image/png;base64,' + encode);
+                
+        //         var imageDownload = canvas.toDataURL();
+        //         downloadURI(imageDownload, "artist.png");
+
+        //         document.getElementById("myImage").setAttribute('src', originImage);
+        //     });
+        // },
         
     }
 }
